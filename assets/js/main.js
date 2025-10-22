e26a1d308b36e6dc9fbecc2e5ae0f617e919f67e
IsaacGReyesD <isaacsreyes57@gmail.com>
2025-10-23 01:05:41 +0200

    Update images and navbar
    The municipalities and their images were arranged for the prototype, and the images of the people were arranged according to their names.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const navbar = document.getElementById('mainNav');
     const hero = document.getElementById('hero');
 
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > hero.offsetHeight) {
-            navbar.classList.add('navbar-sticky');
-        } else {
-            navbar.classList.remove('navbar-sticky');
-        }
-    });
+    if (hero) { // Only run if hero element exists
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > hero.offsetHeight) {
+                navbar.classList.add('navbar-sticky');
+            } else {
+                navbar.classList.remove('navbar-sticky');
+            }
+        });
+    }
 
     // Smooth scroll for nav links
     document.querySelectorAll('.nav-link').forEach(anchor => {
