7cbe6e09610f702c3624164711c47cf1558828eb
IsaacGReyesD <isaacsreyes57@gmail.com>
2025-10-23 00:16:33 +0200

    feat: implement sticky navbar, add content and fix navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     // Navbar scroll effect
-    const navbar = document.querySelector('.navbar');
+    const navbar = document.getElementById('mainNav');
+    const hero = document.getElementById('hero');
+
     window.addEventListener('scroll', () => {
-        if (window.scrollY > 50) {
-            navbar.classList.add('scrolled');
+        if (window.scrollY > hero.offsetHeight) {
+            navbar.classList.add('navbar-sticky');
         } else {
-            navbar.classList.remove('scrolled');
+            navbar.classList.remove('navbar-sticky');
         }
     });
 
